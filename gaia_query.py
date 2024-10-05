f75133f4d5e20916719f9689cd8877345efd7c61
asanzra <asanz2003@gmail.com>
2024-10-05 14:53:54 +0200

    Added Example of a query for gaia

---
+++ gaia_query.py
@@ -0,0 +1,15 @@
+from astroquery.gaia import Gaia
+
+"""
+Query the Gaia archive for all sources within a certain radius from the given point,
+which have parallax below the given limit (within 3 sigma),
+and save the result as a numpy zip archive.
+"""
+job = Gaia.launch_job(
+	"SELECT TOP 10 source_id, ra, dec, phot_g_mean_mag "+
+	"FROM gaiadr3.gaia_source "+
+	"WHERE phot_g_mean_mag < 6 "+
+	"ORDER BY phot_g_mean_mag DESC;"
+)
+table = job.get_results()
+print(table)
