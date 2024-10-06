e38c703dcbbf32157e2d591e227f97262640643a
asanzra <asanz2003@gmail.com>
2024-10-06 16:57:05 +0200

    Capturas

---
+++ Godot_project/info.gd
@@ -0,0 +1,13 @@
+extends Node # Use the appropriate base class (Node2D, Control, etc.)
+
+@onready var picture = $TextureRect # Reference to the TextureRect node
+
+func _ready():
+	picture.visible = false # Ensure the picture is initially hidden
+
+func _process(delta):
+	# Show the picture when the "Space" key is pressed
+	if Input.is_action_pressed("ui_accept"): # Default "ui_accept" maps to Space or Enter
+		picture.visible = true
+	else:
+		picture.visible = false
