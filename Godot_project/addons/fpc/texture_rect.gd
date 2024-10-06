b2a355fd40395613e07d66f8445ed541aa8eeace
Claudia <clagarci@student.42madrid.com>
2024-10-06 12:23:19 +0200

    text and visor coordination

--- Godot_project/addons/fpc/texture_rect.gd
+++ Godot_project/addons/fpc/texture_rect.gd
@@ -1,23 +1,15 @@
-extends Node
+extends TextureRect
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Label.visible_ratio = 0
-	$AnimationPlayer.play("show")
-	#$Countdown2.queue("show")
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	#if $Label.visible_ratio != 1:
-		#$AnimationPlayer.play("show")
-		#$Countdown2.queue("show")
-	#if $Label.visible_ratio == 1:
-		#$Label.hide()
-		
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		$Label.hide()
-		#$Countdown2.play("show")
+		$AnimationPlayer.play("Quitar casco")
